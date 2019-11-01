d7b01754878d7e8b27204d0689b392cc24bae35f
Solomon Tolosa <solomon.tolosa@amobee.com>
2019-10-31 23:23:12 -0400

    BLL and DAL added

---
+++ MyTournament.DAL/TeamDataRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTournament.DAL
+{
+    public class TeamDataRepository
+    {
+        public static void AddTeam(string teamName, string teamId)
+        {
+            //To Do 
+            //Connect to database and add a team
+        }
+
+        //public static Team GetTeam(string teamId)
+        //{
+        //    //get team by Id
+        //}
+
+
+    }
+}
